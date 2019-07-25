4e09ffc422507c36e744e6a11e8b65eb7118c75b
Marlon Monzon <martianworm17@gmail.com>
2019-07-25 16:28:01 +0800

    [WIP] Registration page demo

--- ot-lon-jsp-demo/WebContent/pages/registration-successful.jsp
+++ ot-lon-jsp-demo/WebContent/pages/registration-successful.jsp
@@ -1,8 +1,53 @@
+<%@ page import="com.ot.lon.models.Person" %>
+
 <html>
 	<head>
 		<title>Registration Successful!</title>
 		<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossorigin="anonymous" />
 	</head>
 	<body>
+		<%
+			Person person = (Person) request.getAttribute("person");
+		%>
+	
+		<div class="pure-g">
+		    <div class="pure-u-1-3"></div>
+		    <div class="pure-u-1-3"><h1>Registration Successful!</h1></div>
+		    <div class="pure-u-1-3"></div>
+		</div>
+		
+		<br />
+		<br />
+		
+		<div class="pure-g">
+		    <div class="pure-u-1-3"></div>
+		    <div class="pure-u-1-3"><p>The following are the details of your registration.</p></div>
+		    <div class="pure-u-1-3"></div>
+		</div>
+	
+		<br />
+		<br />
+		
+		<div class="pure-g">
+		    <div class="pure-u-1-3"></div>
+		    
+		    <div class="pure-u-1-3">
+				<b>First Name:</b> <br /><br />
+				<%= person.getFirstName() %><br />
+				<br />
+				
+				
+				<b>Last Name:</b> <br /><br />
+				<%= person.getLastName() %><br />
+				<br />
+				
+			
+			</div>
+		    
+		    <div class="pure-u-1-3"></div>
+		</div>
+		
+		
+	
 	</body>
 </html>
--- ot-lon-jsp-demo/src/com/ot/lon/servlets/RegistrationServlet.java
+++ ot-lon-jsp-demo/src/com/ot/lon/servlets/RegistrationServlet.java
@@ -39,16 +39,16 @@ public class RegistrationServlet extends HttpServlet {
 	
 	private Person buildPerson(HttpServletRequest request) {
 
-		String firstName      = request.getParameter("first_name");
-		String lastName       = request.getParameter("last_name");
-		String birthDate      = request.getParameter("birthdate");
-		String age            = request.getParameter("age");
-		String gender         = request.getParameter("gender");
-		String addressLine1   = request.getParameter("address_line_1");
-		String addressLine2   = request.getParameter("address_line_2");
-		String city           = request.getParameter("city");
-		String country        = request.getParameter("country");
-		String mobileNumber   = request.getParameter("mobile_number");
+		String firstName    = request.getParameter("first_name");
+		String lastName     = request.getParameter("last_name");
+		String birthDate    = request.getParameter("birthdate");
+		String age          = request.getParameter("age");
+		String gender       = request.getParameter("gender");
+		String addressLine1 = request.getParameter("address_line_1");
+		String addressLine2 = request.getParameter("address_line_2");
+		String city         = request.getParameter("city");
+		String country      = request.getParameter("country");
+		String mobileNumber = request.getParameter("mobile_number");
 		
 		return new Person(firstName, lastName, birthDate, age, gender, addressLine1, addressLine2, city, country, mobileNumber);
 	}
