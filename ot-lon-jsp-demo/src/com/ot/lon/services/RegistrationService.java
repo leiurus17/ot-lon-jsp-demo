23ebb0d5c9dd7dadc65e0a34d0c5a3535c16680b
Marlon Monzon <martianworm17@gmail.com>
2019-07-25 17:16:38 +0800

    [WIP] Write details to file

--- ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
+++ ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
@@ -1,10 +1,25 @@
 package com.ot.lon.services;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.ot.lon.models.Person;
 
 public class RegistrationService {
 	
-	public void savePersonToList(Person person) {
+	public static final String PATH_TO_FILE = "C:\\Users\\mmonzon\\Documents\\gh\\ot-lon-jsp-demo\\ot-lon-jsp-demo\\WebContent\\data\\persons.txt";
+	
+	public void savePersonToList(Person person) throws IOException {
+				
+		BufferedWriter writer = new BufferedWriter(new FileWriter(PATH_TO_FILE, true));
+		
+		// TODO fix this
+		writer.append(person.toString());
+		
+		writer.append("\n\n");
+		
+		writer.close();
 		
 	}
 
