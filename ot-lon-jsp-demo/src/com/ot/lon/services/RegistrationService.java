3508e27debed6ecef61a3560fe9b362cc5d9dba0
Marlon Monzon <martianworm17@gmail.com>
2019-07-29 16:47:42 +0800

    Remove comments

--- ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
+++ ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
@@ -9,21 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 import com.ot.lon.models.Person;
 
 public class RegistrationService {
-	
-	//public static final String PATH_TO_FILE = "C:\\data\\persons.txt";
-	
+
 	public static final String PATH_TO_FILE = "C:\\data\\persons.txt";
 
 	public static final String PIPE = "|";
-	
+
 	public void savePersonToList(Person person) throws IOException {
 
-		// TODO fix this
 		if (!person.getFirstName().isEmpty()) {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(PATH_TO_FILE, true));
-			
-			writer.append(PIPE.concat(person.getFirstName()).concat(PIPE)
-					.concat(person.getLastName().concat(PIPE)));	
+
+			writer.append(PIPE.concat(person.getFirstName()).concat(PIPE).concat(person.getLastName().concat(PIPE)));
 			writer.append("\n");
 			writer.close();
 		}
