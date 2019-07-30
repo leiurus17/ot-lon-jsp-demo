4638c80c70c074295f4541fe4a74d73f8b20726d
Marlon Monzon <martianworm17@gmail.com>
2019-07-30 10:16:10 +0800

    Removed unused import

--- ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
+++ ot-lon-jsp-demo/src/com/ot/lon/services/RegistrationService.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.ot.lon.models.Person;
 
 public class RegistrationService {
